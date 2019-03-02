ac3ec8b1395ff4e5f2b38a7e9e677e03641836df
YuriAdonev <zyztem@mail.ru>
2019-03-02 23:16:13 +0700

    Секция сотрудничество

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
   svg4everybody({});
 	
+	// header
+	var headerBg = document.querySelector('.main-header__bg'),
+			headerBgHeight = 0;
+	
+	window.addEventListener('scroll', headerBgScroll, false);
+	
+	function headerBgScroll() {
+		if (window.scrollY > 1) {
+			headerBgHeight = (window.scrollY / 8);
+			if (headerBgHeight >= 82 ) {
+				headerBgHeight = 82;
+			}
+		}
+		headerBg.style.height = headerBgHeight + 'px';
+	};
+	
 	// main-slider
 	var interleaveOffset = 0.7;
 
@@ -45,6 +61,8 @@ $(document).ready(function () {
 	var swiperMain = new Swiper(".main-slider", swiperOptions);
 	
 	$(".section-header").animated();
+	$(".main-products").animated();
+	$(".main-cooperation").animated();
 	$(".main-blog__list").animated();
 	$(".main-blog").animated();
 	
